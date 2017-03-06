023a09f45bff0e7214e226dda89654056d0c36ab
Elton Oliveira <eltonknoxville@hotmail.com>
2017-03-06 19:30:37 -0400

    Tocar de id datatable
    Tocar de id datatable

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -14,7 +14,7 @@
 			<div class="row">
 				<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 					<h2>Boletos em aberto</h2>
-					<table class="table table-striped" id="example">
+					<table class="table table-striped" id="form-admin">
 						<thead>
 							<th>DOCUMENTO</th>
 							<th>NOME SACADO / CPF</th>
