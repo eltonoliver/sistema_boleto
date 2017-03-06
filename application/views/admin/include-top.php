42d27348b06e9b7fafbad49feb46d4145aa92970
Elton Oliveira <eltonknoxville@hotmail.com>
2017-03-06 19:24:44 -0400

    Nome do Administrador
    Adicionei o nome do administrador recuperado de uma session

--- application/views/admin/include-top.php
+++ application/views/admin/include-top.php
@@ -1,7 +1,7 @@
 				<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 top">
 					<div class="row">
 						<div class="col-md-4 txtalg text-uppercase visivel">
-							<h4>Nome do administrador</h4>
+							<h4><?php echo $_SESSION['nome']; ?></h4>
 						</div>
 						<!--
 						<div class="col-md-4 call">
